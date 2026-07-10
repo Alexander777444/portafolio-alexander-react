15eec6de14172d61c6960a3eeb3e06afed45e3d9
Alexander Bolaños <enrique.bolanos6974@alumnos.udg.mx>
2026-07-10 15:17:25 -0600

    feat: Se añadió un fondo de manchas rojas difuminadas y animadas en el portafolio, manteniendo el contraste del texto y dejando el contenido visible por encima.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import BackgroundGlow from './components/BackgroundGlow'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -10,6 +11,7 @@ import Footer from './components/Footer'
 function App() {
   return (
     <>
+      <BackgroundGlow />
       <Navbar />
       <Hero />
       <About />
